48cf4dbb35c815c72cba7b1a73bdffd9a2a0c830
unknown <happy.maulik@yahoo.co.in>
2015-05-17 12:35:32 +0530

    debugEnable js edited

--- debugEnable.js
+++ debugEnable.js
@@ -1,6 +1,6 @@
 if(window.location.search.indexOf("debug=1") < 0){
-window.console = {log: function () {} };
-window.alert = function(){};
+	window.console = {log: function () {} };
+	window.alert = function(){};
 }else if(typeof console == "undefined"){
- window.console = {log: function () {}};
+ 	window.console = {log: function () {}};
 }
